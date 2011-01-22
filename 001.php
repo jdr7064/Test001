6432b878eced127bbeecb1c23478efc4382fb9ba
zheng <jdr7064@gmail.com>
2011-01-22 09:24:49 +0800

    2012-01-22

--- 001.php
+++ 001.php
@@ -15,7 +15,7 @@ $fun=$_GET{'f'};
 $img_height=70;//先定义图片的长、宽
 $img_width=25;
 $authnum='';
-//生产验证码字符
+//生产验证码字符 
 $ychar="0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
 $list=explode(",",$ychar);
 for($i=0;$i<4;$i++){
